a49c2a8d556815e2159cbf7cb510ab62743d3bfc
brotherdrake <n89220628001@gmail.com>
2024-08-07 23:56:01 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+qua_of_hw = 12
+qua_of_hours = 1.5
+name_of_cours = 'Python'
+time_of_ex = qua_of_hours / qua_of_hw
+print('Курс: ' + name_of_cours + ', всего задач:' + str(qua_of_hw) + ', затрачено часов: ' + str(qua_of_hours) + ', среднее время выполнения ' + str(time_of_ex) + ' часа.')
